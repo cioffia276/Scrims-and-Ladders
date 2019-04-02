a2e388c6bc9be86ae4f28bb0752da823e89ab761
zajeskyk <46795530+zajeskyk@users.noreply.github.com>
2019-04-02 11:55:56 -0400

    Update TeamCreator.java

--- TeamCreator.java
+++ TeamCreator.java
@@ -8,17 +8,22 @@ public class TeamCreator {
         Connection c = null;
         Statement stmt = null;
         stmt = c.createStatement();
-        ResultSet rs1 = stmt.executeQuery("SELECT * FROM ____ WHERE _____ = HARDCORE AND RANK = ________" +
+        ResultSet rs1 = stmt.executeQuery("SELECT username.public."userInfo" FROM public."userInfo", public.characteristics" +
+                " WHERE playerType = 'casual' AND rank = 'Gold 4" +
                 " AND ROLE = 'top'");
-        ResultSet rs2 = stmt.executeQuery("SELECT * FROM ____ WHERE _____ = HARDCORE AND RANK = ________" +
+        ResultSet rs2 = stmt.executeQuery("SELECT username.public."userInfo" FROM public."userInfo", public.characteristics" +
+                " WHERE playerType = 'casual' AND rank = 'Gold 4" +
                 " AND ROLE = 'jungle'");
-        ResultSet rs3= stmt.executeQuery("SELECT * FROM ____ WHERE _____ = HARDCORE AND RANK = ________" +
+        ResultSet rs3= stmt.executeQuery("SELECT username.public."userInfo" FROM public."userInfo", public.characteristics" +
+                " WHERE playerType = 'casual' AND rank = 'Gold 4" +
                 " AND ROLE = 'mid'");
-        ResultSet rs4 = stmt.executeQuery("SELECT * FROM ____ WHERE _____ = HARDCORE AND RANK = ________" +
+        ResultSet rs4 = stmt.executeQuery("SELECT username.public."userInfo" FROM public."userInfo", public.characteristics" +
+                " WHERE playerType = 'casual' AND rank = 'Gold 4" +
                 " AND ROLE = 'adc'");
-        ResultSet rs5 = stmt.executeQuery("SELECT * FROM ____ WHERE _____ = HARDCORE AND RANK = ________" +
-                " AND ROLE = 'support");
-        while(rs.next()){
+        ResultSet rs5 = stmt.executeQuery("SELECT username.public."userInfo" FROM public."userInfo", public.characteristics" +
+                " WHERE playerType = 'casual' AND rank = 'Gold 4" +
+                " AND ROLE = 'support'");
+
 
         }
 
