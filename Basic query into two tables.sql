f60a54d52cf03269c8d8451707c28f4a7f883878
Catherine Davis <34278437+Catherine-M-Davis@users.noreply.github.com>
2019-04-23 15:29:35 -0400

    Update Basic query into two tables.sql
    --++ ALTERED TO FIT WHAT IS ON THE DB 4/23

--- Basic query into two tables.sql
+++ Basic query into two tables.sql
@@ -1,22 +1,24 @@
-﻿/*insert into public."userInfo" (password,"f_Name","l_Name","email",player,coach,spectator,manager,gender,"region","username")
+﻿/*insert into userInfo (password,f_Name,l_Name,email,player,coach,spectator,manager,gender,region,username)
 values ('1234','Cat','Davis','cdavis@fake.com','0','1','0','0','1','USA','CDavis159')
 */
 /*
-update public."userInfo" set "player"= 1' where "username" = 'CDavis159'
+update userInfo set player= 1' where username = 'CDavis159'
 */
-select * from public."userInfo";
+select * from userInfo;
 
-select * from public.characteristics;
+select * from characteristics;
 
 /*
-insert into public.characteristics ("role","rank","playerType","experience","s_Laning","w_Laning","s_TFight","w_TFight",
-"s_ShotCalling","w_ShotCalling","s_GSense","w_GSense","s_Vision","w_Vision","preferred_Champion","username")
+insert into characteristics (role,rank,playerType,experience,s_Laning,w_Laning,s_TFight,w_TFight,
+s_ShotCalling,w_ShotCalling,s_GSense,w_GSense,s_Vision,w_Vision,preferred_Champion,username)
 values('King of the castle','Gold 4','Casual','None','1','0','1','0','1','0','1','0','1','0','Olympics','CDavis159')
 */
 
 /*
-UPDATE public.characteristics set "s_Farm" = '1', "w_Farm" = '0' where "username" = 'CDavis159'
+UPDATE characteristics set s_Farm = '1', w_Farm = '0' where username = 'CDavis159'
 */
 
-select * from public."userInfo" ui
-join public.characteristics c on ui."username" = c."username"
+select * from userInfo ui
+join characteristics c on ui.username = c.username
+
+--++ ALTERED TO FIT WHAT IS ON THE DB 4/23
